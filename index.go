b8d258ed48dd4720390e5b5b7a84b9deecad7020
baseigneurie <bseig@outlook.com>
2015-07-23 20:24:43 -0500

    more testing

--- index.go
+++ index.go
@@ -5,19 +5,19 @@ import (
 	"text/template"
 )
 
-func main() {
+func init() {
+	http.Handle("/style/", http.StripPrefix("/style/", http.FileServer(http.Dir("/site.css"))))
 	http.HandleFunc("/", home)
-
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":8080", nil)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-	t, err := template.New("main").Parse("views/main.html")
+	t, err := template.New("main").ParseFiles("views/main.tmpl")
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
 	}
 
-	err = t.ExecuteTemplate(w, "main", "Hello!")
+	err = t.ExecuteTemplate(w, "main", nil)
 
 }
